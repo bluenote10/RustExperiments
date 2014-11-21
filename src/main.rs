a5de4b3ff942d4ab010437c99f137b3b8ae1004c
Fabian <fabian@fabuntu.none>
2014-11-21 13:49:04 +0100

    reference experiments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
-
+#![allow(unused_assignments)]
 
 extern crate riftlib;
 
@@ -9,18 +9,15 @@ use riftlib::glmath;
 
 
 
-
+/// structure used in several tests
 struct Point {
-    x: int,
-    y: int,
-    z: int,
+  x: int,
+  y: int,
 }
 
 
-fn main() {
-
-  let v = glmath::Vec3{x: 0f32, y: 0f32, z: 0f32};
-
+#[test]
+fn stuff_from_the_guide() {
   fn nested(x: int) -> int {
     2*x
   }
@@ -28,7 +25,6 @@ fn main() {
   
   
   let mut point = Point { 
-    z: 0, 
     x: 0, 
     y: 0, 
   };
@@ -36,6 +32,12 @@ fn main() {
   
   let x = 5i;
   let y = 10i;
+
+  fn cmp(a: int, b: int) -> Ordering {
+    if a < b { Less }
+    else if a > b { Greater }
+    else { Equal }
+  }
 
   let z = match cmp(x, y) {
     Less    => println!("less"),
@@ -50,8 +52,68 @@ fn main() {
   }
 }
 
-fn cmp(a: int, b: int) -> Ordering {
-    if a < b { Less }
-    else if a > b { Greater }
-    else { Equal }
+
+#[test]
+fn reference_reassignment() {
+
+  { // i and j immutable
+    let i = 0i;
+    let j = 1i;
+  
+    let mut rf_mu = &i;
+    let     rf_im = &i;
+
+    // rf_im = &j; // error: re-assignment of immutable variable `rf_im`
+    rf_mu = &j;
+    rf_mu = &i; // and back...
+
+    // assigning a constant is not allowed
+    // rf_mu = &42; // error: borrowed value does not live long enough
+    
+    // the mutability of the value depends on the mutability of what we borrowed
+    // *rf_mu = 5; // error: cannot assign to immutable dereference of `&`-pointer `*rf_mu`
+    // *rf_im = 5; // error: cannot assign to immutable dereference of `&`-pointer `*rf_im`
+  }
+  { // in order to borrow &mut, i must be mut itself
+    let i = 0i;
+    // let     r = &mut i; // error: cannot borrow immutable local variable `i` as mutable
+    // let mut r = &mut i; // error: cannot borrow immutable local variable `i` as mutable
+  }
+  { // now with mutable i and j
+    let mut i = 0i;
+    let mut j = 1i;
+    let mut k = 2i;
+  
+    let mut rf_mu = &mut i;
+    //let     rf_im = &mut i; // error: cannot borrow `i` as mutable more than once at a time
+    let     rf_im = &mut j;
+
+    // rf_im = &mut k; // error: re-assignment of immutable variable `rf_im`
+    // rf_mu = &mut i; // error: cannot borrow `i` as mutable more than once at a time 
+    rf_mu = &mut k;
+    
+    *rf_im = 3; // now k = 2 ?
+    // assert_eq!(j, 4) // error: cannot borrow `j` as immutable because it is also borrowed as mutable
+    *rf_mu = 4; // now j = 3 ?
+    // suprisingly, re_mu has borrowed both k and i!
+    //assert_eq!(k, 3) // error: cannot borrow `k` as immutable because it is also borrowed as mutable
+    //assert_eq!(i, 3) // error: cannot borrow `i` as immutable because it is also borrowed as mutable
+    
+    // all these also do not work since we cannot borrow at all!
+    // println!("{}", i);
+    // println!("{}", j);
+    // println!("{}", k);
+  }
+
+
 }
+
+
+
+fn main() {
+
+  let v = glmath::Vec3{x: 0f32, y: 0f32, z: 0f32};
+
+}
+
+
