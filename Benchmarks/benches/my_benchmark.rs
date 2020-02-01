4e1ad9e9fab0ffff70e974d749ecccc7a8085369
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-02-01 16:57:16 +0100

    experiments with criterion

--- Benchmarks/benches/my_benchmark.rs
+++ Benchmarks/benches/my_benchmark.rs
@@ -1,25 +1,97 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+extern crate rand;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId, BatchSize, Throughput};
 
 use std::iter;
+use rand::Rng;
 
-use criterion::BenchmarkId;
-//use criterion::Criterion;
-use criterion::Throughput;
+#[inline]
+fn noop(x: f64) -> f64 {
+    x
+}
 
-fn do_something(x: f64) -> f64 {
-    //x.abs().sin().exp()
+#[inline]
+fn abs1(x: f64) -> f64 {
+    x.abs()
+}
+
+#[inline]
+fn abs2(x: f64) -> f64 {
     if x > 0.0 { x } else { -x }
 }
 
-fn from_elem(c: &mut Criterion) {
-    let size: usize = 1024;
-
+fn bench_old(c: &mut Criterion) {
     for x in [0.0, 1.0, -1.0, 123.456, -123.456].iter() {
         c.bench_with_input(BenchmarkId::new("input_example", x), x, |b, &x| {
-            b.iter(|| do_something(x));
+            b.iter(|| abs2(black_box(x)));
         });
     }
 }
 
-criterion_group!(benches, from_elem);
+fn bench_noop(c: &mut Criterion) {
+    c.bench_function("noop", move |b| {
+        b.iter_batched(|| 0.0, |x| noop(x), BatchSize::SmallInput)
+    });
+
+    let mut rng = rand::thread_rng();
+    c.bench_function("noop (rand)", move |b| {
+        b.iter_batched(|| rng.gen::<f64>(), |x| noop(x), BatchSize::SmallInput)
+    });
+}
+
+fn bench_abs1(c: &mut Criterion) {
+    c.bench_function("abs1 (0.0)", move |b| {
+        b.iter_batched(|| 0.0, |x| abs1(x), BatchSize::SmallInput)
+    });
+    c.bench_function("abs1 (+1.0)", move |b| {
+        b.iter_batched(|| 1.0, |x| abs1(x), BatchSize::SmallInput)
+    });
+    c.bench_function("abs1 (-1.0)", move |b| {
+        b.iter_batched(|| -1.0, |x| abs1(x), BatchSize::SmallInput)
+    });
+    c.bench_function("abs1 (+12345678.90123456)", move |b| {
+        b.iter_batched(|| 12345678.90123456, |x| abs1(x), BatchSize::SmallInput)
+    });
+    c.bench_function("abs1 (-12345678.90123456)", move |b| {
+        b.iter_batched(|| -12345678.90123456, |x| abs1(x), BatchSize::SmallInput)
+    });
+    let mut rng = rand::thread_rng();
+    c.bench_function("abs1 (rand)", move |b| {
+        b.iter_batched(|| rng.gen::<f64>(), |x| abs1(x), BatchSize::SmallInput)
+    });
+}
+
+fn bench_abs2(c: &mut Criterion) {
+    c.bench_function("abs2 (0.0)", move |b| {
+        b.iter_batched(|| 0.0, |x| abs2(black_box(x)), BatchSize::SmallInput)
+    });
+    c.bench_function("abs2 (+1.0)", move |b| {
+        b.iter_batched(|| 1.0, |x| abs2(black_box(x)), BatchSize::SmallInput)
+    });
+    c.bench_function("abs2 (-1.0)", move |b| {
+        b.iter_batched(|| -1.0, |x| abs2(black_box(x)), BatchSize::SmallInput)
+    });
+    c.bench_function("abs2 (+12345678.90123456)", move |b| {
+        b.iter_batched(|| 12345678.90123456, |x| abs2(black_box(x)), BatchSize::SmallInput)
+    });
+    c.bench_function("abs2 (-12345678.90123456)", move |b| {
+        b.iter_batched(|| -12345678.90123456, |x| abs2(black_box(x)), BatchSize::SmallInput)
+    });
+    let mut rng = rand::thread_rng();
+    c.bench_function("abs2 (rand)", move |b| {
+        b.iter_batched(|| rng.gen::<f64>(), |x| abs2(black_box(x)), BatchSize::SmallInput)
+    });
+}
+
+
+criterion_group!(
+    benches,
+    bench_noop,
+    bench_abs1,
+    bench_abs2,
+    bench_old,
+);
 criterion_main!(benches);
+
+// look into: https://github.com/bheisler/criterion.rs/issues/23
+// for per iteration setup
