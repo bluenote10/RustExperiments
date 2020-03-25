8c1f3d5a19e0989bd71000df0aff455697293be4
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-03-25 23:06:55 +0100

    started some benchmarking

--- SortedSlotArray/src/main.rs
+++ SortedSlotArray/src/main.rs
@@ -9,6 +9,13 @@ use sorted_slot_array::vec_set::VecSet;
 
 use pretty_assertions::assert_eq;
 
+
+fn gen_rand_values(n: usize) -> Vec<f64> {
+    let mut rng = rand::thread_rng();
+    let values: Vec<f64> = (0..n).map(|_| rng.gen()).collect();
+    values
+}
+
 macro_rules! create_cmp {
     ($func:ident, $get:ident, $count:ident) => {
         static mut $count: u64 = 0;
@@ -35,7 +42,62 @@ create_cmp!(cmp_c, get_num_calls_c, NUM_CALLS_C);
 create_cmp!(cmp_d, get_num_calls_d, NUM_CALLS_D);
 
 
+fn benchmark_fill_array_tree(values: &[f64]) -> usize {
+    let mut set = ArrayTree::new(cmp_d, 16);
+    for x in values {
+        set.insert(*x);
+    }
+    set.len()
+}
+
+fn benchmark_fill_splay_tree(values: &[f64]) -> usize {
+    let mut set = SplaySet::new(cmp_a);
+    for x in values {
+        set.insert(*x);
+    }
+    set.len()
+}
+
+/*
+struct Benchmark<F>
+where
+    F: Fn(&[f64]) -> usize
+{
+    name: String,
+    func: F,
+}
+*/
+struct Benchmark
+{
+    name: String,
+    func: fn(&[f64]) -> usize,
+}
+
 fn main() {
+
+    let benchmarks: Vec<Benchmark> = vec![
+        Benchmark {
+            name: "SplayTree".to_string(),
+            func: benchmark_fill_splay_tree,
+        },
+        Benchmark {
+            name: "ArrayStump".to_string(),
+            func: benchmark_fill_array_tree,
+        },
+    ];
+
+    for benchmark in benchmarks {
+        println!("Running benchmark: {}", benchmark.name);
+        let n = 1000;
+        let values = gen_rand_values(n);
+        assert_eq!(values.len(), n);
+        let len = (benchmark.func)(&values);
+        assert_eq!(len, n)
+    }
+
+    /*
+    // let a = if 1 < 2 { benchmark_fill_array_tree } else { benchmark_fill_splay_tree };
+    let arr: Vec<fn(&[f64]) -> usize> = vec![benchmark_fill_array_tree, benchmark_fill_splay_tree];
 
     let mut rng = rand::thread_rng();
 
@@ -69,6 +131,7 @@ fn main() {
     assert_eq!(data_a, data_b);
     assert_eq!(data_a, data_c);
     assert_eq!(data_a, data_d);
+    */
 
     println!("Num calls A: {}", get_num_calls_a());
     println!("Num calls B: {}", get_num_calls_b());
