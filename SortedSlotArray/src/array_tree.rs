b65a779d19f925ddf076ffa0ee4366cff8d16da8
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-03-25 08:30:17 +0100

    fixed insertion bug

--- SortedSlotArray/src/array_tree.rs
+++ SortedSlotArray/src/array_tree.rs
@@ -30,6 +30,7 @@ where
     }
 
     pub fn insert(&mut self, t: T) -> bool {
+        // println!("\nInserting: {:?}", t);
         if self.data.len() == 0 {
             self.data.push(self.new_block(t));
             return true;
@@ -50,6 +51,7 @@ where
         } else {
             0
         };
+        // println!("idx_block: {}    block_len: {}", idx_block, self.data[idx_block].len());
 
         // Split block if necessary
         if self.data[idx_block].len() >= self.capacity as usize {
@@ -60,11 +62,14 @@ where
             self.data[idx_block].truncate(tail_from);
             self.data.insert(idx_block + 1, block_tail);
 
+            // println!("block l: {:?}", self.data[idx_block]);
+            // println!("block r: {:?}", self.data[idx_block + 1]);
             // Determine into which of the two split blocks the new value goes.
             // FIXME: Can we miss an "equals" case here if we go into block than doesn't have the equal element?
             if (self.comparator)(&t, &self.data[idx_block + 1][0]) == Ordering::Greater {
                 idx_block += 1;
             }
+            // println!("idx_block: {}", idx_block);
         }
 
         // Binary search for value index
@@ -75,11 +80,14 @@ where
         if equals {
             return false;
         }
+        // println!("idx_value: {}", idx_value);
 
         // Value insert
         let block_len = self.data[idx_block].len();
-        if idx_block < block_len {
+        if idx_value < block_len {
+            // println!("block: {:?}", self.data[idx_block]);
             self.data[idx_block].insert(idx_value, t);
+            // println!("block: {:?}", self.data[idx_block]);
         } else {
             self.data[idx_block].push(t);
         }
@@ -176,6 +184,7 @@ where
 mod test {
     use super::*;
     use std::cmp::Ordering;
+    use pretty_assertions::assert_eq;
     use rand::{Rng, SeedableRng};
     use rand::rngs::StdRng;
 
@@ -210,6 +219,13 @@ mod test {
         at.insert(3);
         assert_eq!(at.data, [vec![1, 2, 3], vec![4, 5]]);
         assert_eq!(at.num_elements, 5);
+    }
+
+    #[test]
+    fn test_array_no_index_hiccup() {
+        let mut at = new_array!(8, vec![vec![2], vec![4], vec![6, 8]]);
+        at.insert(7);
+        assert_eq!(at.data, [vec![2], vec![4], vec![6, 7, 8]]);
     }
 
     #[test]
@@ -256,5 +272,20 @@ mod test {
             insert_many!(at, [1, 2, 3, 4]);
             assert_eq!(at.collect(), [1, 2, 3, 4]);
         }
+    }
+
+    //#[ignore]
+    #[test]
+    fn test_failing() {
+        let mut at = ArrayTree::new(|a: &f64, b: &f64| a.partial_cmp(b).unwrap(), 16);
+        let vals = vec![0.6994135560499647, 0.15138991083383901, 0.17989509662598502, 0.22855960374503625, 0.7394173591733456, 0.8606810583068278, 0.025843624735059523, 0.1416162372765526, 0.9789425643425963, 0.6312677864630949, 0.34678659888024466, 0.7876614416763924, 0.6260871506068197, 0.34733559592131624, 0.5722923635764159, 0.14416998787798063, 0.839158671060864, 0.2621428817535354, 0.9334439919690996, 0.016414089291711065, 0.8795903741012259, 0.051958655798298614, 0.8313985552845266, 0.026928982020677505, 0.779969564116276, 0.6437306675337413, 0.03822809941255523, 0.777911020749552, 0.4639770428538855, 0.7039388191038694, 0.31363729764551374, 0.8111651227165783, 0.5174339383176408, 0.49384841003283086, 0.5214549475595969, 0.0823716635367353, 0.7310183483079477, 0.6196297749276181, 0.6226877845880779, 0.8987550167723078, 0.9536731852226494, 0.2719858776118911, 0.837006810218081, 0.7570466272336563, 0.9649096907962248, 0.09547804495341239, 0.26299769639555115, 0.6883529379785718, 0.23545125345269502, 0.5611223421257663, 0.81145380876482, 0.7821846165410649, 0.8385374221326543, 0.2287909449815878, 0.9938012642875733, 0.30515950398348823, 0.021945251189301795, 0.7456118789178752, 0.24917873250483202, 0.19461925257672297, 0.08596890658908873, 0.8208413553993631, 0.2799020116906893, 0.622583855342935, 0.3406868767224045, 0.7125811318179431, 0.8171813899535424, 0.9875530622413784, 0.8124194427320398, 0.27890169087536465, 0.4582999489551358, 0.8170130026270258, 0.1116683852975886, 0.9523649049789342, 0.1626401579175366, 0.7006463636943299, 0.5396656897339597, 0.73824000529768, 0.8975902131523751, 0.3138666758196337, 0.959190654990596, 0.6786382471256971, 0.8807317907186307, 0.9923109213923168, 0.7704353170122445, 0.20331717853087872, 0.9191784945915048, 0.3458975102965529, 0.44567705127366397, 0.08758863415076357, 0.8940937525362007, 0.2046747373689708, 0.1540080303289173, 0.8088614347095653, 0.09821866105193844, 0.050284880746519045, 0.9585396829998039, 0.35100273069739263, 0.8263845327940142, 0.6305932414080216];
+        for (i, x) in vals.iter().enumerate() {
+            at.insert(*x);
+            let mut expected = vals[0 .. i + 1].to_vec();
+            expected.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            assert_eq!(at.collect(), expected);
+        }
+
+
     }
 }
--- SortedSlotArray/src/main.rs
+++ SortedSlotArray/src/main.rs
@@ -3,6 +3,7 @@ extern crate sorted_slot_array;
 use rand::Rng;
 
 use sorted_slot_array::sorted_array::SortedArray;
+use sorted_slot_array::array_tree::ArrayTree;
 use sorted_slot_array::splay::SplaySet;
 use sorted_slot_array::vec_set::VecSet;
 
@@ -11,6 +12,7 @@ use pretty_assertions::assert_eq;
 static mut NUM_CALLS_A: u64 = 0;
 static mut NUM_CALLS_B: u64 = 0;
 static mut NUM_CALLS_C: u64 = 0;
+static mut NUM_CALLS_D: u64 = 0;
 
 
 fn cmp_a(a: &f64, b: &f64) -> std::cmp::Ordering {
@@ -34,6 +36,13 @@ fn cmp_c(a: &f64, b: &f64) -> std::cmp::Ordering {
     a.partial_cmp(b).unwrap()
 }
 
+fn cmp_d(a: &f64, b: &f64) -> std::cmp::Ordering {
+    unsafe {
+        NUM_CALLS_D += 1;
+    }
+    a.partial_cmp(b).unwrap()
+}
+
 fn main() {
 
     let mut rng = rand::thread_rng();
@@ -44,11 +53,13 @@ fn main() {
     let mut set_a = SplaySet::new(cmp_a);
     let mut set_b = SortedArray::new(cmp_b, 20, 4);
     let mut set_c = VecSet::new(cmp_c, 20);
+    let mut set_d = ArrayTree::new(cmp_d, 16);
 
     for x in &vals {
         set_a.insert(*x);
         set_b.insert(*x);
         set_c.insert(*x);
+        set_d.insert(*x);
     }
 
     set_b.debug();
@@ -56,17 +67,22 @@ fn main() {
     let data_a: Vec<_> = set_a.into_iter().collect();
     let data_b = set_b.collect();
     let data_c = set_c.collect();
+    let data_d = set_d.collect();
 
+    println!("{:?}", vals);
     assert_eq!(data_a.len(), n);
     assert_eq!(data_b.len(), n);
     assert_eq!(data_c.len(), n);
+    assert_eq!(data_d.len(), n);
     assert_eq!(data_a, data_b);
     assert_eq!(data_a, data_c);
+    assert_eq!(data_a, data_d);
 
     unsafe {
         println!("Num calls A: {}", NUM_CALLS_A);
         println!("Num calls B: {}", NUM_CALLS_B);
         println!("Num calls C: {}", NUM_CALLS_C);
+        println!("Num calls D: {}", NUM_CALLS_D);
     }
 
 }
