0125ce0679ec185eda2fc7cf16068f89bcd49808
Fabian Keller <github.100.fkeller@spamgourmet.com>
2022-04-08 21:32:07 +0200

    added compression

--- SerdeChecks/src/main.rs
+++ SerdeChecks/src/main.rs
@@ -1,8 +1,14 @@
 mod types;
 
 use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
 use std::io::Write;
 use std::path::Path;
+use std::path::PathBuf;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
 
 use types::Sequence;
 
@@ -11,12 +17,57 @@ fn load_sequence_from_file(path: &Path) -> Sequence {
     serde_json::from_reader(file).unwrap()
 }
 
-fn get_file_size(path: &Path) -> u64 {
+fn add_extension(path: &Path, extension: impl AsRef<std::path::Path>) -> PathBuf {
+    // https://users.rust-lang.org/t/append-an-additional-extension/23586/12
+    let mut path = path.to_owned();
+    match path.extension() {
+        Some(ext) => {
+            let mut ext = ext.to_os_string();
+            ext.push(".");
+            ext.push(extension.as_ref());
+            path.set_extension(ext)
+        }
+        None => path.set_extension(extension.as_ref()),
+    };
+    path
+}
+
+struct FileSize {
+    raw: u64,
+    zip: u64,
+}
+
+fn get_raw_file_size(path: &Path) -> u64 {
     let file = File::open(path).unwrap();
     file.metadata().unwrap().len()
 }
 
-fn write_as_json(seq: &Sequence) -> u64 {
+fn get_zip_file_size(path: &Path) -> u64 {
+    let file = File::open(path).unwrap();
+    let mut input = BufReader::new(file);
+
+    let path_out = add_extension(path, "gz");
+    {
+        // https://github.com/rust-lang/flate2-rs/blob/master/examples/compress_file.rs
+        // Note that default compression corresponds to level 6:
+        // https://docs.rs/flate2/1.0.22/src/flate2/lib.rs.html#223-225
+        let compression = Compression::default();
+        let output = File::create(&path_out).unwrap();
+        let mut encoder = GzEncoder::new(output, compression);
+        copy(&mut input, &mut encoder).unwrap();
+        encoder.finish().unwrap();
+    }
+    get_raw_file_size(&path_out)
+}
+
+fn get_file_size(path: &Path) -> FileSize {
+    FileSize {
+        raw: get_raw_file_size(path),
+        zip: get_zip_file_size(path),
+    }
+}
+
+fn write_as_json(seq: &Sequence) -> FileSize {
     let path = Path::new("/tmp/test.json");
     {
         serde_json::to_writer(File::create(path).unwrap(), seq).unwrap();
@@ -24,7 +75,7 @@ fn write_as_json(seq: &Sequence) -> u64 {
     get_file_size(path)
 }
 
-fn write_as_pretty_json(seq: &Sequence) -> u64 {
+fn write_as_pretty_json(seq: &Sequence) -> FileSize {
     let path = Path::new("/tmp/test.pretty.json");
     {
         serde_json::to_writer_pretty(File::create(path).unwrap(), seq).unwrap();
@@ -32,7 +83,7 @@ fn write_as_pretty_json(seq: &Sequence) -> u64 {
     get_file_size(path)
 }
 
-fn write_as_msgpack_compact(seq: &Sequence) -> u64 {
+fn write_as_msgpack_compact(seq: &Sequence) -> FileSize {
     let path = Path::new("/tmp/test.compact.msgpack");
     let data = rmp_serde::encode::to_vec(seq).unwrap();
     {
@@ -41,7 +92,7 @@ fn write_as_msgpack_compact(seq: &Sequence) -> u64 {
     get_file_size(path)
 }
 
-fn write_as_msgpack_named(seq: &Sequence) -> u64 {
+fn write_as_msgpack_named(seq: &Sequence) -> FileSize {
     let path = Path::new("/tmp/test.named.msgpack");
     let data = rmp_serde::encode::to_vec_named(seq).unwrap();
     {
@@ -50,7 +101,7 @@ fn write_as_msgpack_named(seq: &Sequence) -> u64 {
     get_file_size(path)
 }
 
-fn write_as_bincode(seq: &Sequence) -> u64 {
+fn write_as_bincode(seq: &Sequence) -> FileSize {
     let path = Path::new("/tmp/test.bincode");
     let data = bincode::serialize(seq).unwrap();
     {
@@ -59,7 +110,7 @@ fn write_as_bincode(seq: &Sequence) -> u64 {
     get_file_size(path)
 }
 
-fn write_as_cbor(seq: &Sequence) -> u64 {
+fn write_as_cbor(seq: &Sequence) -> FileSize {
     let path = Path::new("/tmp/test.cbor");
     {
         ciborium::ser::into_writer(seq, File::create(path).unwrap()).unwrap();
@@ -67,40 +118,42 @@ fn write_as_cbor(seq: &Sequence) -> u64 {
     get_file_size(path)
 }
 
-// rmp_serde::encode::to_vec
-
 fn main() {
     let path = Path::new("test.json");
     let seq = load_sequence_from_file(path);
 
-    let len_reference = 21749;
-
-    let len_orig = get_file_size(path);
-    let len_json = write_as_json(&seq);
-    let len_pretty_json = write_as_pretty_json(&seq);
-    let len_msgpack_compact = write_as_msgpack_compact(&seq);
-    let len_msgpack_named = write_as_msgpack_named(&seq);
-    let len_bincode = write_as_bincode(&seq);
-    let len_cbor = write_as_cbor(&seq);
-
-    let get_ref_size = |len: u64| len as f64 / len_reference as f64;
+    let size_orig = get_file_size(path);
+    let size_json = write_as_json(&seq);
+    let size_pretty_json = write_as_pretty_json(&seq);
+    let size_msgpack_compact = write_as_msgpack_compact(&seq);
+    let size_msgpack_named = write_as_msgpack_named(&seq);
+    let size_bincode = write_as_bincode(&seq);
+    let size_cbor = write_as_cbor(&seq);
 
     let entries = [
-        ("orig", len_orig),
-        ("json", len_json),
-        ("pretty.json", len_pretty_json),
-        ("msgpack (compact)", len_msgpack_compact),
-        ("msgpack (named)", len_msgpack_named),
-        ("bincode", len_bincode),
-        ("cbor", len_cbor),
+        ("orig", size_orig),
+        ("json", size_json),
+        ("pretty.json", size_pretty_json),
+        ("msgpack (compact)", size_msgpack_compact),
+        ("msgpack (named)", size_msgpack_named),
+        ("bincode", size_bincode),
+        ("cbor", size_cbor),
     ];
+
+    let size_reference = 21749;
+    let size_reference_zip = 4038;
+    let get_ratio_to_ref = |len: u64| len as f64 / size_reference as f64;
+    let get_ratio_to_ref_zip = |len: u64| len as f64 / size_reference_zip as f64;
 
     for (name, len) in entries {
         println!(
-            "{:<20} {:10} {:8.3}",
+            "{:<20} {:10} {:8.3} {:10} {:8.3} {:8.3}",
             name.to_owned() + ":",
-            len,
-            get_ref_size(len)
+            len.raw,
+            get_ratio_to_ref(len.raw),
+            len.zip,
+            get_ratio_to_ref(len.zip),
+            get_ratio_to_ref_zip(len.zip),
         );
     }
 }
