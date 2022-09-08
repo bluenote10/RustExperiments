1a442103c50c87f2ee6ed9c064d06afc58431fb9
Fabian Keller <github.100.fkeller@spamgourmet.com>
2022-09-08 20:17:11 +0200

    first output generation

--- torch-tch/src/main.rs
+++ torch-tch/src/main.rs
@@ -1,38 +1,37 @@
 use anyhow::bail;
-use tch::{vision::imagenet, Tensor};
+use tch::{Device, Kind, Tensor};
 
 ///
-/// cargo run -- ~/gdrive/colab/ddsp_pytorch/output/debug/state.pth /tmp/out
+/// https://docs.rs/tch/latest/tch/index.html
+/// https://github.com/LaurentMazare/tch-rs/tree/main/examples/jit
 ///
-/// Internal torch error: PytorchStreamReader failed locating file constants.pkl: file not found
-/// https://stackoverflow.com/questions/69979034/runtimeerror-pytorchstreamreader-failed-locating-file-data-pkl-file-not-found
+/// cargo run -- ~/git/_ext/ddsp_pytorch/export/ddsp_debug_pretrained.ts
 ///
-
-
-fn hello_world() {
-    let t = Tensor::of_slice(&[3, 1, 4, 1, 5]);
-    let t = t * 2;
-    t.print();
-}
-
-/*
-https://github.com/LaurentMazare/tch-rs/tree/main/examples/jit
-*/
 
 pub fn main() -> anyhow::Result<()> {
     let args: Vec<_> = std::env::args().collect();
-    let (model_file, image_file) = match args.as_slice() {
-        [_, m, i] => (m.to_owned(), i.to_owned()),
-        _ => bail!("usage: main model.pt image.jpg"),
+    let model_file = match args.as_slice() {
+        [_, m] => (m.to_owned()),
+        _ => bail!("usage: main <TORCHSCRIPT-MODEL>"),
     };
     let model = tch::CModule::load(model_file)?;
-    /*
-    let output = model
-        .forward_ts(&[image.unsqueeze(0)])?
-        .softmax(-1, tch::Kind::Float);
-    for (probability, class) in imagenet::top(&output, 5).iter() {
-        println!("{:50} {:5.2}%", class, 100.0 * probability)
-    }
-    */
+
+    println!("is_cuda {}", Device::cuda_if_available().is_cuda());
+
+    let device = Device::Cpu;
+    let num_steps = 100;
+
+    let pitches = Tensor::rand(&[num_steps], (Kind::Float, device));
+    let pitches = pitches.reshape(&[1, -1, 1]);
+    let loudness = Tensor::rand(&[num_steps], (Kind::Float, device));
+    let loudness = loudness.reshape(&[1, -1, 1]);
+    println!("loudness.shape = {:?}", loudness.size());
+    println!("pitches.shape = {:?}", pitches.size());
+
+    let output = model.forward_ts(&[&pitches, &loudness])?;
+    // output.print();
+
+    println!("output.shape = {:?}", output.size());
+
     Ok(())
 }
