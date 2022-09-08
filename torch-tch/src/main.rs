9990303e5cafb05fc95ae572e416d8bc5af8e6f3
Fabian Keller <github.100.fkeller@spamgourmet.com>
2022-09-08 20:23:25 +0200

    rough throughput estimation

--- torch-tch/src/main.rs
+++ torch-tch/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use anyhow::bail;
 use tch::{Device, Kind, Tensor};
 
@@ -16,22 +18,37 @@ pub fn main() -> anyhow::Result<()> {
     };
     let model = tch::CModule::load(model_file)?;
 
-    println!("is_cuda {}", Device::cuda_if_available().is_cuda());
+    println!("is_cuda: {}", Device::cuda_if_available().is_cuda());
 
     let device = Device::Cpu;
+
+    let num_frames = 10;
     let num_steps = 100;
 
-    let pitches = Tensor::rand(&[num_steps], (Kind::Float, device));
-    let pitches = pitches.reshape(&[1, -1, 1]);
-    let loudness = Tensor::rand(&[num_steps], (Kind::Float, device));
-    let loudness = loudness.reshape(&[1, -1, 1]);
-    println!("loudness.shape = {:?}", loudness.size());
-    println!("pitches.shape = {:?}", pitches.size());
+    let t_start = Instant::now();
+    let mut num_samples = 0;
+    for _ in 0..num_frames {
+        let pitches = Tensor::rand(&[num_steps], (Kind::Float, device));
+        let pitches = pitches.reshape(&[1, -1, 1]);
+        let loudness = Tensor::rand(&[num_steps], (Kind::Float, device));
+        let loudness = loudness.reshape(&[1, -1, 1]);
+        println!("loudness.shape = {:?}", loudness.size());
+        println!("pitches.shape = {:?}", pitches.size());
 
-    let output = model.forward_ts(&[&pitches, &loudness])?;
-    // output.print();
+        let output = model.forward_ts(&[&pitches, &loudness])?;
+        // output.print();
 
-    println!("output.shape = {:?}", output.size());
+        println!("output.shape = {:?}", output.size());
+        num_samples += output.size3()?.1;
+    }
+
+    let elapsed = t_start.elapsed();
+    println!("Total samples: {}", num_samples);
+    println!("Total time: {}", elapsed.as_secs_f64());
+    println!(
+        "Throughput: {} kb / sec",
+        (num_samples as f64) / 1000.0 / elapsed.as_secs_f64()
+    );
 
     Ok(())
 }
