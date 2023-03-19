5e2f70cdacaf8d9ce57f9c649e14c60fa76facc2
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-03-19 10:59:02 +0100

    some first experiments with dioxus

--- GuiExperiments/dioxus-hello-world/src/main.rs
+++ GuiExperiments/dioxus-hello-world/src/main.rs
@@ -1,11 +1,18 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
 
+use crate::utils::console_log;
+
+mod utils;
+
 fn main() {
     dioxus_web::launch(App);
 }
 
 fn App(cx: Scope) -> Element {
+    console_log!("Hello {}!", "world");
+    console_log!("2 + 4 = {}", 2 + 4);
+
     cx.render(rsx! {
         div {
             "Hello, world!"
