3bdd1e19a3a7ca4ecda83c531a1804c1fbf8232b
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-03-21 22:15:38 +0100

    wry hello world

---
+++ GuiExperiments/wry-hello-world/src/main.rs
@@ -0,0 +1,31 @@
+use wry::{
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+};
+
+fn main() -> wry::Result<()> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Hello World")
+        .build(&event_loop)?;
+    let _webview = WebViewBuilder::new(window)?
+        .with_url("file:///home/fabian/git/RustExperiments/GuiExperiments/wry-hello-world/web/build/index.html")?
+        .build()?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
