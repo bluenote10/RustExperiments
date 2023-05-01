b457fa32a57fbb2fd228ca90e004087de4324ae7
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-05-01 15:12:30 +0200

    some first tests for proc macro

--- GuiExperiments/leptos-alternate-view/src/macro_impl.rs
+++ GuiExperiments/leptos-alternate-view/src/macro_impl.rs
@@ -1,25 +1,85 @@
 extern crate proc_macro;
 use proc_macro2::{Delimiter, Group, TokenStream, TokenTree};
 use quote::{quote, quote_spanned};
-use syn::{spanned::Spanned, Expr};
+use syn::{
+    parse::{Parse, ParseStream},
+    spanned::Spanned,
+    Expr, Ident, LitStr, Result, Token,
+};
 
 pub fn comp(input: TokenStream) -> TokenStream {
     println!("raw input:\n{:#?}", input);
 
-    let input: TokenTree = Group::new(Delimiter::Parenthesis, input).into();
-    let input: TokenStream = input.into();
+    // let input: TokenTree = Group::new(Delimiter::Parenthesis, input).into();
+    // let input: TokenStream = input.into();
+    // println!("wrapped input:\n{:#?}", input);
 
-    println!("wrapped input:\n{:#?}", input);
+    match parse_input(input) {
+        Ok(comp_body) => {
+            println!("{:#?}", comp_body);
 
+            quote! {
+                log!("Hello world from macro, using parent scope symbol: {}", style);
+            }
+            .into()
+        }
+        Err(err) => {
+            //quote_spanned!(err.span() => compile_error!("Could not parse token stream as expression")).into()
+            err.into_compile_error()
+        }
+    }
+
+    /*
     let input_span = input.span();
     let Ok(s) = syn::parse2::<Expr>(input) else {
         return quote_spanned!(input_span => compile_error!("Could not parse token stream as expression")).into()
     };
+    */
+}
 
-    println!("{:#?}", s);
+fn parse_input(input: TokenStream) -> Result<CompBody> {
+    syn::parse2::<CompBody>(input)
+}
 
-    quote! {
-        log!("Hello world from macro, using parent scope symbol: {}", style);
+#[derive(Debug)]
+struct CompBody {
+    expressions: Vec<Expr>,
+}
+
+impl Parse for CompBody {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let expressions: Vec<_> = input
+            .parse_terminated(Expr::parse, Token![,])?
+            .into_iter()
+            .collect();
+        Ok(CompBody { expressions })
     }
-    .into()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let stream: TokenStream = quote!();
+        let result = parse_input(stream).unwrap();
+        assert_eq!(result.expressions.len(), 0);
+
+        let stream: TokenStream = quote!(foo);
+        let result = parse_input(stream).unwrap();
+        assert_eq!(result.expressions.len(), 1);
+
+        let stream: TokenStream = quote!(foo,);
+        let result = parse_input(stream).unwrap();
+        assert_eq!(result.expressions.len(), 1);
+
+        let stream: TokenStream = quote!(foo, bar);
+        let result = parse_input(stream).unwrap();
+        assert_eq!(result.expressions.len(), 2);
+
+        let stream: TokenStream = quote!(foo, bar,);
+        let result = parse_input(stream).unwrap();
+        assert_eq!(result.expressions.len(), 2);
+    }
 }
