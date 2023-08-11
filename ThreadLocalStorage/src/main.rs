5fbb9b2562c23033b7f18f1198c1b413cfca4aa5
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-08-11 22:26:59 +0200

    more experiments

--- ThreadLocalStorage/src/main.rs
+++ ThreadLocalStorage/src/main.rs
@@ -1,7 +1,75 @@
-use std::cell::RefCell;
+#![allow(dead_code)]
+
+use std::cell::{Cell, RefCell};
+use std::marker::PhantomData;
+use std::sync::MutexGuard;
 use std::thread;
 
-fn fake_static() -> i32 {
+fn is_sync<T: Sync>(_x: T) {}
+fn is_send<T: Send>(_x: T) {}
+
+struct NotSendNotSync {
+    _marker: PhantomData<*const ()>,
+}
+
+impl NotSendNotSync {
+    fn share(&self) -> NotSendNotSync {
+        NotSendNotSync {
+            _marker: PhantomData,
+        }
+    }
+}
+
+struct NotSync {
+    _marker: PhantomData<Cell<()>>,
+}
+
+struct NotSend {
+    _marker: PhantomData<MutexGuard<'static, ()>>,
+}
+
+fn check() {
+    // is_send(NotSendNotSync {
+    //     _marker: PhantomData,
+    // });
+    // is_sync(NotSendNotSync {
+    //     _marker: PhantomData,
+    // });
+
+    is_send(NotSync {
+        _marker: PhantomData,
+    });
+    // is_sync(NotSync {
+    //     _marker: PhantomData,
+    // });
+
+    // is_send(NotSend {
+    //     _marker: PhantomData,
+    // });
+    is_sync(NotSend {
+        _marker: PhantomData,
+    });
+}
+
+fn fake_static_not_sync_not_send() -> NotSendNotSync {
+    thread_local!(static INSTANCE : NotSendNotSync = NotSendNotSync{ _marker: PhantomData});
+    INSTANCE.with(|instance| instance.share())
+}
+
+fn fake_static_not_sync_not_send_lazy() -> NotSendNotSync {
+    // Following the pattern: https://stackoverflow.com/a/76879555/1804173
+    use once_cell::unsync::Lazy;
+
+    thread_local! {
+        static INSTANCE: Lazy<NotSendNotSync> = Lazy::new(||
+            NotSendNotSync{ _marker: PhantomData}
+        );
+    }
+
+    INSTANCE.with(|instance| instance.share())
+}
+
+fn fake_static_i32() -> i32 {
     thread_local!(static COUNTER : i32 = 0);
     COUNTER.with(|c| {
         println!("{}", c);
@@ -12,10 +80,10 @@ fn fake_static() -> i32 {
 thread_local!(static FOO: RefCell<u32> = RefCell::new(1));
 
 fn main() {
-    println!("{}", fake_static());
+    println!("{}", fake_static_i32());
 
     let t1 = thread::spawn(move || {
-        println!("{}", fake_static());
+        println!("{}", fake_static_i32());
     });
     t1.join().unwrap();
 
