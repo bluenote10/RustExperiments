acebae10e610567a20a41ee1aaa2da320e3748f2
Fabian Keller <github.100.fkeller@spamgourmet.com>
2024-06-04 23:21:46 +0200

    fix project setup

--- simple_app/example/example_1.py
+++ simple_app/example/example_1.py
@@ -2,3 +2,5 @@ import simple_app
 
 print(simple_app.__path__)
 print(dir(simple_app))
+
+simple_app.regular_function()
---
+++ simple_app/python/simple_app/__init__.py
@@ -0,0 +1,6 @@
+from ._simple_app import sum_as_string
+
+
+def regular_function():
+    print("Hello, world")
+    print(sum_as_string(1, 2))
